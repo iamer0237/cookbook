6cc7731d3174fe75a3bfc1133d500c54cff93ef6
Simran <Simran.Mangukiya95@gmail.com>
2022-09-06 16:49:54 +0200

    add file

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css';
+import { client } from './client';
+import Posts from './components/Posts'
+
+class App extends React.Component {
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    client.getEntries()
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          articles: response.items
+        })
+      })
+      .catch(console.error)
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='container'>
+          <header>
+            <div className='wrapper'>
+              <div className='wrapper_inner'>
+              <span className='wrapper_span'>Cookbook</span>
+
+              </div>
+            </div>
+          </header>
+          <main>
+            <div className='wrapper'>
+              <Posts posts={this.state.articles} />
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
