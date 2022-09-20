d8939e5061984023ff9c433dcefc59cb50ba2c59
Ibraheem Amer <iamer0237@gmail.com>
2022-09-20 08:44:34 +0200

    modified some files

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import * as contentful from 'contentful'
-
-export const client = contentful.createClient({
-    space: 'hzt0irk2g9lr',
-    accessToken: 'qEgV4Nub28vz2bwiOLxq6TSNuIv4gkQLAaeB2nO9QF4'
-})
